3bccceadacfed724324c34848173df8bf5eb267f
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-10-27 12:31:09 +0100

    Made basic modal that opens and closes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Modal from './components/Modal/modal';
 
-function App() {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    show: false
+  };
+
+  showModal = x => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+        <button onClick={x => {
+          this.showModal(x);
+        }}>Show yourself Modal!</button>
+        <Modal onClose={this.showModal} show={this.state.show}>Modal message</Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Modal extends Component {
+    onClose = x => {
+        this.props.onClose && this.props.onClose(x);
+    };
+
     render() {
+        if(!this.props.show) {
+            return null;
+        }
+
         return (
             <div>
-                <p>Hi! I'm a Modal</p>
+                <div>{this.props.children}</div>
+                <div>
+                    <button onClick={this.onClose}>Close</button>
+                </div>
             </div>
         )
     }
