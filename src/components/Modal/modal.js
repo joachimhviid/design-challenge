fc3d9eeb38c0d7cdb344a8071564355d914b0e8c
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-10-27 11:45:33 +0100

    Initial setup and added modal component

---
+++ src/components/Modal/modal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Modal extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hi! I'm a Modal</p>
+            </div>
+        )
+    }
+}
