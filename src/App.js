4f1ff68c9cd02c2e2199b588a1a026cfb9336475
Joachim <47396357+jokkejok123@users.noreply.github.com>
2019-10-28 17:34:29 +0100

    Added mobile responsiveness and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './main.css';
 import Modal from './components/Modal/modal';
-import Leaves from './assets/leaves.jpg'
+
+//images
+import PlantDirt from './assets/plant-dirt.jpg';
+import Bonsai from './assets/bonsai.jpg';
+import Bird from './assets/bird-flower.jpg';
+import Glass from './assets/glass-eco.jpg';
+import Bulb from './assets/lightbulb-plant.jpg';
+import PotPlant from './assets/potted-plant.jpg';
 
 class App extends React.Component {
   state = {
@@ -24,22 +31,25 @@ class App extends React.Component {
       <div>
         <div className="container">
           <h1>Hello <span className="orange">Morningtrain</span>!</h1>
-          <h2>Frontend design challenge: Case #1</h2>
+          <h2>Frontend design challenge: <span className="orange">Case #1</span></h2>
         </div>
         <div className="button-container">
-          <button className="toggle-button" onClick={this.showModal('show1')}>Show yourself Modal 1!</button>
-          <button className="toggle-button" onClick={this.showModal('show2')}>Show yourself Modal 2!</button>
-          <button className="toggle-button" onClick={this.showModal('show3')}>Show yourself Modal 3!</button>
-          <button className="toggle-button" onClick={this.showModal('show4')}>Show yourself Modal 4!</button>
-          <button className="toggle-button" onClick={this.showModal('show5')}>Show yourself Modal 5!</button>
-          <button className="toggle-button" onClick={this.showModal('show6')}>Show yourself Modal 6!</button>
+          <button className="toggle-button" id="dirt" onClick={this.showModal('show1')}>Table plant</button>
+          <button className="toggle-button" id="bonsai" onClick={this.showModal('show2')}>Bonsai tree</button>
+          <button className="toggle-button" id="bird" onClick={this.showModal('show3')}>Bird on flower</button>
+          <button className="toggle-button" id="glass" onClick={this.showModal('show4')}>Glass ecosystem</button>
+          <button className="toggle-button" id="bulb" onClick={this.showModal('show5')}>Lightbulb sprout</button>
+          <button className="toggle-button" id="potted" onClick={this.showModal('show6')}>Potted plant</button>
         </div>
-        <Modal onClose={this.showModal('show1')} show={this.state.show1} title="Test modal 1" id="1">Bacon ipsum dolor amet pork chop ball tip frankfurter salami biltong. Meatloaf shankle capicola tail doner ribeye swine tenderloin t-bone pork chop burgdoggen biltong prosciutto frankfurter. Frankfurter andouille meatloaf spare ribs. Pastrami filet mignon pork belly picanha ball tip beef.</Modal>
-        <Modal onClose={this.showModal('show2')} show={this.state.show2} title="Test modal 2" id="2"><img src={Leaves} alt=""/></Modal>
-        <Modal onClose={this.showModal('show3')} show={this.state.show3} title="Test modal 3" id="3">Number 3</Modal>
-        <Modal onClose={this.showModal('show4')} show={this.state.show4} title="Test modal 4" id="4">Number 4</Modal>
-        <Modal onClose={this.showModal('show5')} show={this.state.show5} title="Test modal 5" id="5">Number 5</Modal>
-        <Modal onClose={this.showModal('show6')} show={this.state.show6} title="Test modal 6" id="6">Number 6</Modal>
+        <Modal onClose={this.showModal('show1')} show={this.state.show1} title="Table plant" id="1"><p>Small sprout planted in dirt on a table.</p><img src={PlantDirt} alt=""/></Modal>
+        <Modal onClose={this.showModal('show2')} show={this.state.show2} title="Bonsai tree" id="2"><p>A small bonsai tree.</p><img src={Bonsai} alt=""/></Modal>
+        <Modal onClose={this.showModal('show3')} show={this.state.show3} title="Bird on flower" id="3"><p>A red bird resting on a red flower.</p><img src={Bird} alt=""/></Modal>
+        <Modal onClose={this.showModal('show4')} show={this.state.show4} title="Glass ecosystem" id="4"><p>Ecosystem in a bottle.</p><img src={Glass} alt=""/></Modal>
+        <Modal onClose={this.showModal('show5')} show={this.state.show5} title="Lightbulb sprout" id="5"><p>A seed sprouting inside a lightbulb.</p><img src={Bulb} alt=""/></Modal>
+        <Modal onClose={this.showModal('show6')} show={this.state.show6} title="Potted plant" id="6"><p>A simple potted plant.</p><img src={PotPlant} alt=""/></Modal>
+        <div className="container">
+          <p className="small-text">Made by Joachim Hviid</p>
+        </div>
       </div>
     );
   }
